c726baf2ebc9402c39fc49caae5cfb759b822686
rewjb <rewjb@naver.com>
2020-01-05 11:42:17 +0900

    리액트 + 노드 + 도커 실습 예제

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const server = app.listen(3000, function(){
+    console.log("Express server has started on port 3000")
+});
+
+app.set('views', __dirname + '/');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+/** API 서버 확인용 */
+app.use( '/' ,express.static(__dirname + '/'));
+app.get('/',function(req,res){
+    res.render('index.html');
+});
